fd78b7ab799a6748946d6d184cc938a9cb2c55a4
marius <mariusdotspinu@gmail.com>
2016-03-28 01:09:04 +0300

    Modiffied caesar.py

--- PythonExercises/Caesar cipher/caesar.py
+++ PythonExercises/Caesar cipher/caesar.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 # *-* coding: UTF-8 *-*
-"""Împăratul a primit serie de mesaje importante pe care este
-important să le descifreze cât mai repede.
-Din păcate mesagerul nu a apucat să îi spună împăratul care au fost
-cheile alese pentru fiecare mesaj și tu ai fost ales să descifrezi
-misterul.
-Informații:
-    În criptografie, cifrul lui Caesar este o metodă simplă de a cripta
-un mesaj prin înlocuirea fiecărei litere cu litera de pe poziția aflată
-la un n pași de ea în alfabet (unde este n este un număr întreg cunoscut
-"""
-# pylint: disable=unused-argument
 
 from __future__ import print_function
 
 
-def decripteaza_mesajul(mesaj):
-    """Funcția va primi un mesaj criptat folosind cifrul lui Caesar și
-    va încearca să îl decripteze.
-    """
-    list_chars_of_message = list(mesaj)
+def decrypt_message(message):
+
+    list_chars_of_message = list(message)
 
     key = ord(list_chars_of_message[0]) - ord('a')
 
@@ -52,15 +39,15 @@ def decripteaza_mesajul(mesaj):
 def main():
     """ Main function docstring """
     try:
-        fisier = open("mesaje.secret", "r")
-        mesaje = fisier.read()
-        fisier.close()
+        my_file = open("mesaje.secret", "r")
+        messages = my_file.read()
+        my_file.close()
     except IOError:
-        print("Nu am putut obține mesajele.")
+        print("Could not get files.")
         return
 
-    for mesaj in mesaje.splitlines():
-        decripteaza_mesajul(mesaj)
+    for message in messages.splitlines():
+        decrypt_message(message)
 
 
 if __name__ == "__main__":
