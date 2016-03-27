bda6d6160c015b7875dd5de05f0461a64d1dd00a
marius <mariusdotspinu@gmail.com>
2016-03-28 01:18:00 +0300

    Modiffied to_icao.py

--- PythonExercises/Icao/to_icao/to_icao.py
+++ PythonExercises/Icao/to_icao/to_icao.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 # *-* coding: UTF-8 *-*
-"""
-Organizaţia Internaţională a Aviaţiei Civile propune un alfabet în care
-fiecărei litere îi este asignat un cuvânt pentru a evita problemele în
-înțelegerea mesajelor critice.
-Pentru a se păstra un istoric al conversațiilor s-a decis transcrierea lor
-conform următoarelor reguli:
-    - fiecare cuvânt este scris pe o singură linie
-    - literele din alfabet sunt separate de o virgulă
-Următoarea sarcină ți-a fost asignată:
-    Scrie un program care să primească un fișier ce conține mesajul
-    ce trebuie transmis și generează un fișier numit mesaj.icao ce
-    va conține mesajul scris folosin alfabetul ICAO.
-Mai jos găsiți un dicționar ce conține o versiune a alfabetului ICAO:
-"""
-# pylint: disable=unused-argument
 
 ICAO = {
     'a': 'alfa', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo',
@@ -27,10 +12,6 @@ ICAO = {
 
 
 def icao():
-    """Funcția va primi calea mesajul ce trebuie transmis și
-    va genera un fișier numit mesaj.icao_intrare ce va conține
-    mesajul scris folosind alfabetul ICAO.
-    """
 
     try:
         my_input_file = open("input_icao", "r")
